bc8c6b8d701ab46b91d7cf08ea9c0c9804abfa77
psveerr <129594567+psveerr@users.noreply.github.com>
2024-11-25 12:15:11 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISK_USAGE=$(df -h / | tail -1 | awk '{print $5}' | cut -d'%' -f1)
-if [ "$DISK_USAGE" -gt 10 ]; then
-    echo "WARNING: Disk usage is above 10% ($DISK_USAGE%)"
+if [ "$DISK_USAGE" -gt 80 ]; then
+    echo "WARNING: Disk usage is above 80% ($DISK_USAGE%)"
     exit 1
 else
     echo "Disk usage is normal: $DISK_USAGE%"
